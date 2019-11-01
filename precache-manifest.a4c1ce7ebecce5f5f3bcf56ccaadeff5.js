19416302cb33aabeaf091b0168219e63e9afe62f
JaimeSerrano15 <00025318@uca.edu.sv>
2019-11-01 11:41:33 -0600

    Updates

---
+++ precache-manifest.a4c1ce7ebecce5f5f3bcf56ccaadeff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74efe425e13b61bc5bfa128ad040dd2",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "af1a44397951ef2d18e3",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "af1a44397951ef2d18e3",
+    "url": "/tictactoe/static/js/main.54e8973d.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
